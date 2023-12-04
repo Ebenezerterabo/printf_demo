d6e9f99df6dfad6ced0b21c8189650d9e3b45c00
Ebenezer terabo <teraboebenezer@gmail.com>
2023-12-04 01:49:15 +0000

    Header file updated

--- main.h
+++ main.h
@@ -4,14 +4,14 @@
 /* Standard C library */
 #include <stdarg.h>
 #include <unistd.h>
-#include <string.h>
-#include <stdio.h>
 
 
 /* Function Prototypes */
 int _printf(const char *format, ...);
 int print_char(va_list ap);
 int print_str(va_list ap);
-
+int check_cases(va_list ap, const char *format);
+int _strlen(char *str);
+int _putchr(char c);
 
 #endif
